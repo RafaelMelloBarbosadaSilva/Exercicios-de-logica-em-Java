662a634d16a57b4b35ddd549bbeece0901de2d5f
Rafael Mello Barbosa da Silva <rafamelloba@gmail.com>
2026-03-04 15:03:04 -0300

    Exercicio 7 realizado

---
+++ src/src/Exercicio7.java
@@ -0,0 +1,36 @@
+/* 7 - Faça um algoritmo que leia dois valores booleanos (lógicos) e determine se ambos são VERDADEIRO ou FALSO. */
+
+
+import java.util.Scanner;
+
+public class Exercicio7 {
+    public static boolean lervalor(Scanner scanner){
+        System.out.println("Digite o valor lógico: ");
+
+        while (!scanner.hasNextBoolean()) {
+            System.out.println("Insira um valor válido");
+            scanner.next();
+            System.out.println("Digite o valor lógico: ");
+        }
+        return scanner.nextBoolean();
+    }
+    public static void teste(boolean valor1, boolean valor2){
+        if (valor1 && valor2){
+            System.out.println("Ambos sao verdadeiros");
+        } else if (!valor1 && !valor2) {
+            System.out.println("Ambos sao falsos");
+        } else if (valor1) {
+            System.out.println("o primeiro valor é verdadeiro e o segundo é falso");
+        } else {
+            System.out.println("o primeiro valor é falso e o segundo é verdadeiro");
+        }
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        boolean valor1 = lervalor(scanner);
+        boolean valor2 = lervalor(scanner);
+
+        teste(valor1, valor2);
+    }
+}
